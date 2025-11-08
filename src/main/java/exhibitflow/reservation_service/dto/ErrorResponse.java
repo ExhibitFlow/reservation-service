3eb19f4d10a92a4f002be2d140299fc50858d234
Chandula Jayathilake <114409648+ChandulaJ@users.noreply.github.com>
2025-11-08 22:50:44 +0530

    exception add

---
+++ src/main/java/exhibitflow/reservation_service/dto/ErrorResponse.java
@@ -0,0 +1,17 @@
+package exhibitflow.reservation_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorResponse {
+    private int status;
+    private String message;
+    private String error;
+    private long timestamp;
+}
